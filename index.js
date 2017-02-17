89a8121277e0410a363ddd5a0cd235afd6c0ac19
chichong <wongchichong@gmail.com>
2017-02-17 10:55:02 +0800

    Initial typescript port

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-module.exports = function(arr, obj){
-  if (arr.indexOf) return arr.indexOf(obj);
-  for (var i = 0; i < arr.length; ++i) {
-    if (arr[i] === obj) return i;
-  }
-  return -1;
-};
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function indexof(arr, obj) {
+        if (arr.indexOf)
+            return arr.indexOf(obj);
+        for (var i = 0; i < arr.length; ++i) {
+            if (arr[i] === obj)
+                return i;
+        }
+        return -1;
+    }
+    exports_1("indexof", indexof);
+    return {
+        setters: [],
+        execute: function () {
+            ;
+        }
+    };
+});
